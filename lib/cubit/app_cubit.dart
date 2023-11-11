31309bdc3d83df61ed9328bc72117e687b8a317e
bankoleifeoluwa <ibhankz007@gmail.com>
2023-11-12 00:40:07 +0100

    finishing the details page, and working  on state

---
+++ lib/cubit/app_cubit.dart
@@ -0,0 +1,5 @@
+import 'package:bloc/bloc.dart';
+
+class AppCubits extends Cubit {
+  AppCubits(initialState) : super(initialState);
+}
--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cubit/misc/color.dart';
+import 'package:flutter_cubit/widgets/app_buttons.dart';
 import 'package:flutter_cubit/widgets/app_large_text.dart';
 import 'package:flutter_cubit/widgets/app_text.dart';
+import 'package:flutter_cubit/widgets/responsive_button.dart';
 
 class DetailPage extends StatefulWidget {
   const DetailPage({Key? key}) : super(key: key);
@@ -10,6 +13,8 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  int gottenStars = 3;
+  int selectedIndex = -1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,6 +61,7 @@ class _DetailPageState extends State<DetailPage> {
                             topLeft: Radius.circular(20),
                             topRight: Radius.circular(20))),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -64,7 +70,141 @@ class _DetailPageState extends State<DetailPage> {
                               text: "Yosemite",
                               color: Colors.black87,
                             ),
-                            AppLargeText(text: "#250")
+                            AppLargeText(text: "\$250")
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              color: AppColors.mainColor,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            AppText(
+                              text: "Usa, California",
+                              color: AppColors.mainTextColor,
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          children: [
+                            Wrap(
+                              children: List.generate(
+                                  5,
+                                  (index) => Icon(
+                                        Icons.star,
+                                        color: index < gottenStars
+                                            ? Colors.yellow
+                                            : AppColors.textColor2,
+                                      )),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            AppText(
+                              text: "(4.0)",
+                              color: AppColors.textColor2,
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 25,
+                        ),
+                        AppLargeText(
+                          text: "People",
+                          color: Colors.black.withOpacity(0.8),
+                          size: 20,
+                        ),
+                        SizedBox(height: 10),
+                        AppText(
+                          text: "Number of people in your group",
+                          color: AppColors.textColor1,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Wrap(
+                          children: List.generate(
+                              5,
+                              (index) => InkWell(
+                                    onTap: () {
+                                      setState(() {
+                                        selectedIndex = index;
+                                      });
+                                    },
+                                    child: Container(
+                                      margin: const EdgeInsets.only(right: 10),
+                                      child: AppButtons(
+                                        isIcon: false,
+                                        size: 50,
+                                        backgroundColor: selectedIndex == index
+                                            ? Colors.black
+                                            : Colors.white,
+                                        color: selectedIndex == index
+                                            ? Colors.white
+                                            : Colors.black,
+                                        borderColor: AppColors.buttonBackground,
+                                        text: (index + 1).toString(),
+                                      ),
+                                    ),
+                                  )),
+                        ),
+                        SizedBox(
+                          height: 25,
+                        ),
+                        Column(
+                          children: [
+                            AppLargeText(
+                              text: "Description",
+                              color: Colors.black.withOpacity(0.8),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Column(
+                          children: [
+                            AppText(
+                              text:
+                                  "Yosemite national park is located in central sierra in the US State of California, it is located near the wild protected areas.",
+                              size: 20,
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Column(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.only(right: 20),
+                              child: Row(
+                                children: [
+                                  AppButtons(
+                                      isIcon: true,
+                                      icon: Icons.favorite_border,
+                                      size: 60,
+                                      color: Colors.black,
+                                      backgroundColor: Colors.white,
+                                      borderColor: Colors.black),
+                                  SizedBox(
+                                    width: 15,
+                                  ),
+                                  ResponsiveButton(
+                                    isResponsive: true,
+                                  )
+                                ],
+                              ),
+                            )
                           ],
                         )
                       ],
